22d3f2cbb6236a5257b836b48f9f92e052edb1f8
Emem <ememl@emem.com>
2025-07-02 07:39:32 +0800

    prep for deployment

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', "/authify/login");
+// Route::redirect('/', "/authify/login");
 
-Route::get('/authify/login', [AuthController::class, 'loginForm'])->name('sso.login');
-Route::post('/authify/login', [AuthController::class, 'Login']);
-Route::get('/authify/logout', [AuthController::class, 'logout'])->name('sso.logout');
+// Route::get('/authify/login', [AuthController::class, 'loginForm'])->name('sso.login');
+// Route::post('/authify/login', [AuthController::class, 'Login']);
+// Route::get('/authify/logout', [AuthController::class, 'logout'])->name('sso.logout');
+
+Route::redirect('/', " /login");
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('sso.login');
+Route::post(' /login', [AuthController::class, 'Login']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('sso.logout');
